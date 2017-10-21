d73732ec60c0c50fa061dec144da8cf1e7f5946a
Kateryna <kateryna.bohatova@gmail.com>
2017-10-21 15:24:27 +0300

    Homework_5_21102017

--- Alert.js
+++ Alert.js
@@ -1,15 +1,24 @@
 'use strict';
-var a = 10;
-var b = 2;
-var result1 = a + b;
-var result2 = a - b;
-var result3 = a * b;
+var c = 15;
+var d = 2;
+var result = c + d;
+alert(result);
 
-alert( result1 );
-alert( result2 );
-alert( result3 );
+var a = 10, b = 2, c = 5;
+alert( a + b + c );
 
+var a = 17, b = 10, c = a - b, d = 7;
+var result = c + d;
+alert ( result );
 
+var str = 'Hello, World!';
+alert (str);
 
+var a = "Hello, " + "World!";
+alert( a );
 
+var name = 'Kateryna';
+alert('Hello, ' + name);
 
+var age = 33;
+alert('I am ' + age + " years old");
