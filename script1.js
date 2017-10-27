fbf85572051692efddda25a944f1e0a9f32b0e16
Kateryna <kateryna.bohatova@gmail.com>
2017-10-27 21:24:38 +0300

    Homework_7_27102017

--- script1.js
+++ script1.js
@@ -1,74 +1,17 @@
 'use strict';
 
-var arr = [],
-    x = '',
-    i;
-for (var i = 0; i < 10; i++) {
-    x += 'x'
-    arr.push(str);
-}
-console.log(arr);
+var goods = [1, 4, 6, 7, 8],
+    lastItem = goods[goods.length - 1];
+alert(lastItem);
 
-function arrayFill(s, count) {
-    var i, res = '';
-    for (var i = 0; i < count; i++) {
-        res += s;
-    }
-    return res;
-}
+var goods = ['Привет', 'мир', 'чайки', 'вороны'];
+    goods.push("Компьютер");
+alert(goods);
 
-var i,
-    arr = [];
-for (i = 1; i <= 9; i++){
-    arr.push(arrayFill(i, i));
-}
-console.log(arr);
+var styles = ['Джаз', 'Блюз'];
+    styles.push('Рок-н-ролл');
+    styles[styles.length - 2] = 'Классика';
+    alert(styles.shift());
+    styles.unshift("Рэп", "Регги");
+    alert(styles);
 
-function arrayFill(s, count) {
-        var i, res = '';
-    for (var i = 0; i < count; i++) {
-        res += s;
-    }
-    return res;
-}
-console.log(arrayFill('123-', 10));
-
-function func(arr) {
-    var sum = 0;
-    for (var i = 0 ; i < arr.length; i++) {
-        sum += arr[i];
-        if (sum > 10) {
-            return i + 1;
-        }
-    }
-}
-console.log(func([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-
-
-
-var arr = [1, 3, 5, 6, 7, 9];
-var result = [];
-for (var i = arr.length - 1; i >= 0; i--) {
-    result.push(arr[i]);
-}
-console.log(result);
-
-var arr = [[1, 2, 3], [4, 5], [6]];
-var sum = 0;
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-        sum += arr[i][j];
-    }
-}
-alert(sum);
-
-var arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
-var sum = 0;
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-        for (var k = 0; k < arr[i][j].length; k++) {
-            sum += arr[i][j][k];
-        }
-    }
-}
-alert(sum);
