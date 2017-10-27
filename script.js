b94dac4bb63397ef6c25826ffee4e3197ceb1a83
Kateryna <kateryna.bohatova@gmail.com>
2017-10-27 11:54:27 +0300

    Lesson_6_24102017

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 'use strict';
 
-var op1 = document.getElementById('op1'),
-    op2 = document.getElementById('op2'),
-    result = document.getElementById('result'),
-    buttons = document.getElementsByTagName('button'),
-    i;
-
-function onClick(event) {
-    switch (event.target.innerHTML) {
-        case '+': result.innerHTML = parseFloat(op1.value) + parseFloat(op2.value); break;
-        case '-': result.innerHTML = parseFloat(op1.value) - parseFloat(op2.value); break;
-        case '*': result.innerHTML = parseFloat(op1.value) * parseFloat(op2.value); break;
-        case '/': result.innerHTML = parseFloat(op1.value) / parseFloat(op2.value); break;
+document.write
+for (var i = 2; i<= 10; i++) {
+    document.write("<div style='float: left; width: 100px;'>");
+    for (var j = 2; j <= 10; j++) {
+        document.write(i + '*' + j + ' = ' + i * j + '<br />');
     }
+    document.write('</div>');
 }
 
-for (i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = onClick;
+var str = '';
+for (var i = 1; i < 10; i++) {
+    str += i;
+}
+alert(str);
+
+
+var str = '';
+for (var i = 9; i > 0; i--) {
+    str += i;
+}
+alert(str);
+
+var str = '-';
+for (var i = 1; i < 10; i++) {
+    str += i + '-';
+}
+alert(str);
+
+var str = '';
+for (var i = 1; i <= 20; i++) {
+    document.write("<div style='float: left; width: 400px;'>");
+    str += 'x';
+    document.write(str + '<br>');
 }
 
+for (var i = 1; i <= 9; i++) {
+    for (var j = 1; j <= i; j++) {
+        document.write(i);
+    }
+    document.write('<br>');
+}
 
-
-
+var str = '';
+for(var i = 0; i <= 5; i++) {
+    str += 'xx';
+    document.write(str + '<br>');
+}
