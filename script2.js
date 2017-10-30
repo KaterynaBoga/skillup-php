c989f855790c57c33f7a92a34c15c3589b569e28
Kateryna <kateryna.bohatova@gmail.com>
2017-10-30 23:27:45 +0200

    Homework_7_30102017

---
+++ script2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mas = [15, 'hello', 'trump', 23, 'world', 999, 176];
+for (var i = 0; i < mas.length; i++) {
+    document.write("---" + mas[i] +  "<br />");
+}
+
+var person = {};
+person.name = 'Petya';
+person.age = 17;
+person.gender = 'man';
+person.number = 647;
+alert( person.name + ': ' + person.age + ', ' +  person.gender + ', ' + person.number);
+
