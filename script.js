11b12ea7b309c207d64ac99106a1eccc9622d4a5
Kateryna <kateryna.bohatova@gmail.com>
2017-10-31 21:28:47 +0200

    Homework_8(1-13)_3102017

--- script.js
+++ script.js
@@ -1,51 +1,18 @@
 'use strict';
 
 $(document).ready(function () {
+//    $('h2').html('!');
+    $('h2').css({color: 'blue', 'font-size': '30px'})
+    $('h2 i').css({color: 'red'});
+    $('h2 > i').css({color: 'red'});
+    $('h3').css({color: 'red'});
+    $('.test').css('color', 'red');
+    $('p + p').css('color', 'red');
+    $('li + li.www').css('color', 'red')
+//    $(':header').css('color', 'red');
+//    $('h2:not(.test)').css('color', 'red');
+//    $(':header:not(.test)').css('color', 'red');
 
-    var $op1 = $('#op1'),
-        $op2 = $ ('#op2'),
-        $result = $('#result');
-
-    function onClick() {
-        var op1 = parseFloat($op1.val()),
-            op2 = parseFloat($op2.val());
-        switch ($(this).html()) {
-            case '+': $result.html(op1 + op2); break;
-            case '-': $result.html(op1 - op2); break;
-            case '*': $result.html(op1 * op2); break;
-            case '/': $result.html(op1 / op2); break;
-        }
-    }
-
-    $('button').click(onClick);
-
-    $('#slide').click(function () {
-        $('#panel').slideToggle();
-        $(this).toggleClass('active');
-    });
-
-    $('#hide').click(function() {
-        var $panel = $('#panel');
-
-        if ($panel.is(':visible')) {
-            $panel.animate({opacity: "hide"}, "slow");
-        } else {
-              $panel.animate({opacity: "show"}, "slow");
-    }
-    $(this).toggleClass('active');
-    });
-
-    $('#load').click(function(event){
-        event.preventDefault();
-        var target = $(this).attr('target'),
-            href = $(this).attr('href');
-
-//        $(target).load(href);
-
-    $.get(href, function (content) {
-        $(target).html(content);
-        });
-    });
 });
 
 
