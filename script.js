7c26aa596b68ef726e49c8f5423ce2f61382f28e
Kateryna <kateryna.bohatova@gmail.com>
2017-11-01 20:33:36 +0200

    Homework_8(1-13)_3102017

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 'use strict';
 
 $(document).ready(function () {
-//    $('h2').html('!');
-    $('h2').css({color: 'blue', 'font-size': '30px'})
-    $('h2 i').css({color: 'red'});
-    $('h2 > i').css({color: 'red'});
-    $('h3').css({color: 'red'});
-    $('.test').css('color', 'red');
-    $('p + p').css('color', 'red');
-    $('li + li.www').css('color', 'red')
+//   $('h2').html('!');
+   $('h2').css({color: 'blue', 'font-size': '30px'})
+   $('h2 i').css({color: 'red'});
+   $('h2 > i').css({color: 'red'});
+   $('h3').css({color: 'red'});
+   $('.test').css('color', 'red');
+   $('p + p').css('color', 'red');
+   $('li + li.www').css('color', 'red');
 //    $(':header').css('color', 'red');
 //    $('h2:not(.test)').css('color', 'red');
 //    $(':header:not(.test)').css('color', 'red');
-
+//    $(':header + :header').css('color', 'red');
 });
 
 
