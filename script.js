e65d067d1ea84caab7bf3952c9bda9bd0fb950bd
Kateryna <kateryna.bohatova@gmail.com>
2017-11-01 20:50:34 +0200

    Homework_8(16-23)_01112017

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 'use strict';
 
 $(document).ready(function () {
-//   $('h2').html('!');
-   $('h2').css({color: 'blue', 'font-size': '30px'})
-   $('h2 i').css({color: 'red'});
+   $('p:first').css('color','red');
+   $('p:last').css('color','red');
+//   $('h2:has(i)').css('color','red');
    $('h2 > i').css({color: 'red'});
-   $('h3').css({color: 'red'});
-   $('.test').css('color', 'red');
-   $('p + p').css('color', 'red');
-   $('li + li.www').css('color', 'red');
-//    $(':header').css('color', 'red');
-//    $('h2:not(.test)').css('color', 'red');
-//    $(':header:not(.test)').css('color', 'red');
-//    $(':header + :header').css('color', 'red');
+//   $('li:eq(3)').css('color', 'red');
+   $('li:lt(10)').css('color', 'red');
+
 });
 
 
