e7c010d43c519bb1cc4bcb5d00c4ddf9fb3625df
Kateryna <kateryna.bohatova@gmail.com>
2017-11-10 21:32:31 +0200

    Lesson_11_10112017

---
+++ table.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Таблица</title>
+    <style>
+        td
+        {
+            width: 100px;
+            border: 1px solid black;
+        }
+
+    </style>
+</head>
+<body>
+
+<table>
+    <tr>
+        <td>
+
+        </td>
+        <td>
+
+        </td>
+        <td>
+
+        </td>
+        <td>
+
+        </td>
+    </tr>
+</table>
+</body>
+</html>
