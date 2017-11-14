4d11675a7a470932391431c83e4233b16147f051
Kateryna <kateryna.bohatova@gmail.com>
2017-11-14 23:28:14 +0200

    Homework_12_14112017_4

--- User.php
+++ User.php
@@ -15,61 +15,27 @@ class Worker
 
     private $salary = 0;
 
+    public function __construct($name, $age, $salary)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
     public function getName()
     {
         return $this->name;
     }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
     public function getAge()
     {
         return $this->age;
     }
-
-    public function setAge($age)
-    {
-        if ($this->checkAge($age)) {
-            $this->age = $age;
-        }
-    }
-
-    private function checkAge($age)
-    {
-        if ($age > 1 && $age < 100) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public function getSalary()
     {
         return $this->salary;
     }
 
-    public function setSalary($salary)
-    {
-        $this->salary = $salary;
-    }
-
-
 }
 
-$user1 = new Worker;
-$user1->setName('Ivan');
-$user1->setAge(255);
-$user1->setSalary(1000);
-
-$user2 = new Worker;
-$user2->setName('Vasya');
-$user2->setAge(26);
-$user2->setSalary(2000);
-
-
-echo $user1->getSalary() + $user2->getSalary() . PHP_EOL;
-echo $user1->getAge() + $user2->getAge() . PHP_EOL;
+$user = new Worker('Коля', 25, 1000);
+echo $user->getAge() + $user->getSalary() . PHP_EOL;
 ?>
