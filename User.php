43918274b9feb61b2e09e1e3b15d45ad3cb42d1a
Kateryna <kateryna.bohatova@gmail.com>
2017-11-15 22:15:06 +0200

    Homework_12_14112017_5a

--- User.php
+++ User.php
@@ -5,20 +5,17 @@
  * Date: 14.11.2017
  * Time: 19:27
  */
-include 'Worker.php';
-include 'Student.php';
 
 class User
 {
-
-    protected $name = '';
-
-    protected $age = 0;
+    protected $name;
+    protected $age;
 
     public function getName()
     {
-        return $this->age;
+        return $this->name;
     }
+
     public function setName($name)
     {
         $this->name = $name;
@@ -28,9 +25,89 @@ class User
     {
         return $this->age;
     }
+
     public function setAge($age)
     {
         $this->age = $age;
     }
+
 }
+
+class Worker extends User
+{
+    private $salary;
+
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    public function setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+
+}
+
+$worker1 = new Worker();
+$worker1->setSalary(1000);
+$worker1->setName('Коля');
+$worker1->setAge(25);
+
+$worker2 = new Worker();
+$worker2->setSalary(2000);
+$worker2->setName('Вася');
+$worker2->setAge(26);
+
+echo $worker1->getSalary() + $worker2->getSalary() . PHP_EOL;
+
+class Student extends User
+{
+    private $course;
+    private $cash;
+
+    public function getCourse()
+    {
+        return $this->course;
+    }
+
+    public function setCourse($course)
+    {
+        $this->course = $course;
+    }
+
+    public function getCash()
+    {
+        return $this->cash;
+    }
+
+    public function setCash($cash)
+    {
+        $this->cash = $cash;
+    }
+
+}
+
+$student1 = new Student();
+$student1->setName('Коля');
+$student1->setAge(25);
+$student1->setCourse(3);
+$student1->setCash(1200);
+
+$student2 = new Student();
+$student2->setName('Вася');
+$student2->setAge(26);
+$student2->setCourse(4);
+$student2->setCash(1600);
+
+echo $student1->getName();
+echo $student1->getAge();
+echo $student1->getCourse();
+echo $student1->getCash() . PHP_EOL;
+
+echo $student2->getName();
+echo $student2->getAge();
+echo $student2->getCourse();
+echo $student2->getCash() . PHP_EOL;
+
 
--- Worker.php
+++ Worker.php
@@ -7,28 +7,3 @@
  */
 
 
-class Worker extends User
-{
-    private $salary = 0;
-
-    public function getSalary()
-    {
-        return $this->salary;
-    }
-
-    public function setSalary($salary)
-    {
-        $this->salary = $salary;
-    }
-}
-$worker1 = new Worker();
-$worker1->setName('Ivan');
-$worker1->setAge(25);
-$worker1->setSalary(1000);
-
-$worker2 = new Worker();
-$worker2->setName('Vasya');
-$worker2->setAge(26);
-$worker2->setSalary(2000);
-
-echo $worker1->setSalary() + $worker2->setSalary() . PHP_EOL;
