712e249139f7caee4a8ee94d78aaf2486c9bd74c
Kateryna <kateryna.bohatova@gmail.com>
2017-11-28 21:07:41 +0200

    Lasson_16_28112017

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 include 'Worker.php';
 
 $pdo = new PDO('mysql:host=localhost;dbname=skillup;charset=utf8','root','');
-$sql = 'SELECT * FROM workers WHERE salary = :salary';
+
+/**$sql = 'SELECT * FROM workers WHERE id = :id'; */
+/**$sql = 'SELECT * FROM workers WHERE salary = :salary'; */
+$sql = 'SELECT * FROM workers WHERE age = :age';
 
 /** @var @PDOStatement $result */
 $result = $pdo->prepare($sql);
-$result->execute(['salary' => 500]);
+
+/**$result->execute(['id' => 3]); */
+/**$result->execute(['salary' => 1000]); */
+$result->execute(['age' => 23]);
 
 while ($row = $result->fetchObject(Worker::class)) {
     var_dump($row);
